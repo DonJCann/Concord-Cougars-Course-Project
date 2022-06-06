3ee2341bb2d0362deda123b689ca94a4ea16c3bd
DonJCann <donjcann@gmail.com>
2022-06-06 09:28:13 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Concord_Cougars_Course_Project.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
