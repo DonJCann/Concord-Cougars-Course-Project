7561477f562d7f79fe150a1d6d44903ed35e04aa
DonJCann <donjcann@gmail.com>
2022-06-19 11:38:44 -0400

    User Story 8 Updates

---
+++ Controllers/SwimmerController.cs
@@ -0,0 +1,26 @@
+﻿using Concord_Cougars_Course_Project.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Concord_Cougars_Course_Project.Controllers
+{
+    [Authorize(Roles="Swimmer")]
+    public class SwimmerController : Controller
+    {
+        private readonly SwimSchoolDbContext db;
+        public SwimmerController(SwimSchoolDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
